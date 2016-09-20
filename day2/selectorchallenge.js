6913df3d48a06d141effd47d727580943a1bda24
RobertWhite7 <robertwhite@RobertWhiteMBP.local>
2016-09-20 10:01:47 -0400

    done the correct way

--- day2/selectorchallenge.js
+++ day2/selectorchallenge.js
@@ -1,8 +1,20 @@
-    $("#button").click( function()
-           {
-             alert('button clicked');
-           }
-        );
-
-    $("h3")
-$( "li:first" ).css( "font-style", "italic" );
+//the correct way   
+ $(function(){
+     //modify the paragraph
+ $("#myParagraph").css("font-style", "italic");
+     //Select all h3 tags and modify them
+$("h3").attr("font-style", "bold");
+     //Select first iem in the list and modify it in some way
+ $("#listOfItems  li").first().attr("font-style", "color: green;");
+});
+ 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+ 
