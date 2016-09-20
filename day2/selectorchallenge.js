599c14a65d622b8dc890a47f01dc6cd5a628068d
RobertWhite7 <robertwhite@RobertWhiteMBP.local>
2016-09-20 10:02:51 -0400

    changed a text color

--- day2/selectorchallenge.js
+++ day2/selectorchallenge.js
@@ -1,7 +1,7 @@
 //the correct way   
  $(function(){
      //modify the paragraph
- $("#myParagraph").css("font-style", "italic");
+ $("#myParagraph").css("color", "blue");
      //Select all h3 tags and modify them
 $("h3").attr("font-style", "bold");
      //Select first iem in the list and modify it in some way
