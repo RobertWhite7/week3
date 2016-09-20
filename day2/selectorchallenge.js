1910147992946963f4a270aa8919cd8a66f6fd9b
RobertWhite7 <robertwhite@RobertWhiteMBP.local>
2016-09-20 10:17:47 -0400

    made a few syntax changes

--- day2/selectorchallenge.js
+++ day2/selectorchallenge.js
@@ -3,17 +3,15 @@
      //modify the paragraph
  $("#myParagraph").css("color", "blue");
      //Select all h3 tags and modify them
-$("h3").attr("font-style", "bold");
+$("h3").css("bold");
      //Select first iem in the list and modify it in some way
- $("#listOfItems  li").first().attr("font-style", "color: green;");
+ $("#listOfItems  li").first().css("color", "green");
 });
  
+
    
    
-   
-   
-   
-   
+
    
    
    
